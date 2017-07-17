aa8f1e707f1215fa869f399bc32b32dbf089f137
fransbernhard <mimilundberg@icloud.com>
2017-07-17 17:22:28 +0200

    more testing adding

--- backstop_data/casper_scripts/onReady.js
+++ backstop_data/casper_scripts/onReady.js
@@ -3,4 +3,9 @@ module.exports = function (casper, scenario, vp) {
     // Your web-app is now loaded. Edit here to simulate user interacions or other state changes.
   });
   console.log('onReady.js has run for: ', vp.name);
+  var email = 'alsdjkfkasjdfkj' + Math.floor((Math.random() * 100000) + 1) + '@gmail.com'
+  casper.fillSelectors('form', {
+    'input[placeholder= "email"]': email,
+    'textarea[placeholder= "meddelande"]': 'hej jag är ett test',
+  }, true)
 };
