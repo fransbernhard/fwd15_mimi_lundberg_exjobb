f44591b5aba5bb1be46e15ce9f795a96bd586ef7
fransbernhard <mimilundberg@icloud.com>
2017-07-18 17:52:27 +0200

    fix backatopjs

--- backstop_data/casper_scripts/onReady.js
+++ backstop_data/casper_scripts/onReady.js
@@ -1,11 +1,12 @@
 module.exports = function (casper, scenario, vp) {
-  casper.evaluate(function () {
-    // Your web-app is now loaded. Edit here to simulate user interacions or other state changes.
-  });
-  console.log('onReady.js has run for: ', vp.name);
-  var email = 'alsdjkfkasjdfkj' + Math.floor((Math.random() * 100000) + 1) + '@gmail.com'
+  // casper.evaluate(function () {
+  //   // Your web-app is now loaded. Edit here to simulate user interacions or other state changes.
+  // });
+  // console.log('onReady.js has run for: ', vp.name);
+  
+  // var email = 'alsdjkfkasjdfkj' + Math.floor((Math.random() * 100000) + 1) + '@gmail.com'
   casper.fillSelectors('form', {
-    'input[placeholder= "email"]': email,
+    'input[placeholder= "email"]': 'email@email.com',
     'textarea[placeholder= "meddelande"]': 'hej jag är ett test',
   }, true)
 };
