07b82043feee317315330b749ce443b55e98e5c4
fransbernhard <mimilundberg@icloud.com>
2018-12-26 22:18:27 +0100

    M

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = new mysqli("127.0.0.1", "root", "mimi", "mimi");
-$message = $con->query("SELECT message FROM myTable")->fetch_object()->message;
-$con->close();
-echo "$message <br/>";
-echo "Hello From Sites Folder!";
-phpinfo();
